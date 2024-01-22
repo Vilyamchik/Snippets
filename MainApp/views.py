01bd2a03bbe23d3e9e625f39fa78602d164c7d9a
Vilyamov Maxim <moscow.idol.com@mail.ru>
2024-01-22 18:26:22 +0300

    vnes izmen

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from MainApp.models import Snippet
 
 
 def index_page(request):
@@ -15,3 +16,10 @@ def add_snippet_page(request):
 def snippets_page(request):
     context = {'pagename': 'Просмотр сниппетов'}
     return render(request, 'pages/view_snippets.html', context)
+
+
+def snippet(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    return render(request, 'snippet.html', {'snippet': snippet})
+
+
